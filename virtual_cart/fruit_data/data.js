27314000a4d7e1a12958d3f67e7652ab2987aba5
GurpreetSingh380 <74468946+GurpreetSingh380@users.noreply.github.com>
2024-01-30 20:23:39 +0530

    Clean-1

---
+++ virtual_cart/fruit_data/data.js
@@ -0,0 +1,7 @@
+export const fruits = [
+    { fruit: "Banana", url: "../images/Banana.jpg" },
+    { fruit: "Cabbage", url: "../images/Cabbage.jpg" },
+    { fruit: "Potato", url: "../images/Potato.jpg" },
+    { fruit: "Radish", url: "../images/Radish.jpg" },
+    { fruit: "Capsicum", url: "../images/Capsicum.jpg" },
+  ];
